36e8e690b2c48ff5918875534a8e64442ba175db
travelingdoodle <reggiebrown@gmail.com>
2017-09-02 09:30:01 -0500

    good enough

--- keys.js
+++ keys.js
@@ -1,10 +1,19 @@
-```JavaScript
-console.log('this is loaded');
+
+console.log('keys.js loaded, like a f*#%ing BOSS');
 
 exports.twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: 'A8ZhJiRofGDo9UXsiWuk4Pcdy',
+  consumer_secret: 'TMWucI9EqzzWp3YpmSw3WfEecCYm7JyApSrIo7cAl58xo1GtOp',
+  access_token_key: '119568465-RhZFYJGDPyshEdZ6VTO7c3vo8Gu6iaKUgskQaveN',
+  access_token_secret: 'EgyRBKwemtnpnrVhiz7TIuWzmcWwUQDcskTwP96y7Xun1',
 }
-```
+
+
+// exports.spotifyKeys = {
+//   clientId : '866c82ba1b9b4955b95ab35bdfc76ec2',
+//   clientSecret: 'd156764673ea477a9125510bc802f35c',
+// }
+
+// exports.omdbKeys = {
+//   consumer_key: '40e9cece',
+// }
